bb0f39a3db5a8e3fcc6a1ccf0e5290f9d6531323
aadi <adityamacromaniax@gmail.com>
2021-09-23 20:47:04 +0530

    main.c

--- main.c
+++ main.c
@@ -53,19 +53,52 @@ main( int argc, char * argv[] )
 		fprintf(stderr,"usage: %s <filename>\n",pgm);
 		exit(1);
 	}
-	
+	strcpy(pgm,argv[1]);
+	if(strchr(pgm,'.') == NULL )
+		strcat(pgm,".tny");
+	source = fopen(pgm,"r");
+	if(source == NULL){
+		fprintf(stderr,"File %s not found\n",pgm);
+		exit(1);
+	}
+	listing = stdout; /* send listing to  screen */
+	fprintf(listing,"\n TINY COMPILATION: %s\n",pgm);
 
-
-
-
-
-
-
-
-
-
-
-
-
+#if NO_PARSE
+	while (getToken() != ENDFILE);
+#else
+	syntaxTree = parse();
+	if(TraceParse){
+		fprintf(listing,"\nSyntax tree: \n");
+		printTree(syntaxTree);
+	}
+#if !NO_ANALYZE 
+	if(! Error)
+	{	fprintf(listing,"\n Building Symbol Table...\n");
+		buildSymtab(syntaxTree);
+		fprintf(listing,"\nChecking Types...\n");
+		typeCheck(syntaxTree);
+		fprintf(listing,"\nType Checking Finished\n");
+	}
+#if !NO_CODE
+	if(! Error)
+	{	char * codefile;
+		int fnlen = strcspn(pgm,".");
+		codefile = (char *) calloc (fnlen+4, sizeof(char));
+		strncpy(codefile,pgm,fnlen);
+		strcat(codefile,".tm");
+		code = fopen(codefile,"w");
+		if(code == NULL)
+		{	printf("Unable to open %s\n",codefile);
+			exit(1);
+		}
+		codeGen(syntaxTree,codefile);
+		fclose(code);
+	}
+#endif
+#endif
+#endif
+	return 0;
+}
 
 
